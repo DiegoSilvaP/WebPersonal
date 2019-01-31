7c07fb0a450189bab0ee51a60bbe8a1027f0d675
Diego Silva <alienware.matrix@gmail.com>
2019-01-31 00:53:43 -0600

    Procfile and import heroku

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 # le indica a django donde buscar / guardar los archivos en el servidor de depuración
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
